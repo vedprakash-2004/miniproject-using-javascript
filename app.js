15dfaff93903c57292a8fe8a348c53d1fc66fe33
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-19 16:48:17 +0530

    next step Flash Button & Level Up

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let gameSeq=[];
 let userSeq=[];
 
+let btns = ["yellow","red","purple","green"];
+
 let started = false;
 let level = 0;
 
@@ -15,7 +17,24 @@ document.addEventListener("keypress", function(){
     }
 });
 
+function btnFlash(btn){
+    btn.classList.add("flash");
+    setTimeout(function(){
+        btn.classList.remove("flash");
+    },1000);
+
+}
+
 function levelUp() {
     level++;
     h2.innerText = `Level ${level}`;
+
+    //random btn choose
+    let randIdx = Math.floor(Math.random()*3);
+    let randColor = btns[randIdx];
+    let randbtn = document.querySelector(`.${randColor}`);
+    console.log(randIdx);
+    console.log(randColor);
+    console.log(randbtn);
+    btnFlash(random);
 }
